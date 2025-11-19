dd12bbdbf2528cc4b94621cf3841155f054a0303
Mikael <174565360+Mikael92002@users.noreply.github.com>
2025-11-19 16:20:13 -0600

    Restructured design

---
+++ script.js
@@ -0,0 +1,16 @@
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("intersecting");
+//       return;
+//     }
+//   });
+// });
+
+// // all divs:
+
+// const divs = document.querySelectorAll("div");
+
+// divs.forEach((div)=>{
+//     observer.observe(div);
+// })
